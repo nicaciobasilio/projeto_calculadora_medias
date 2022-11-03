057ff68eef6ed0978e87e67a583913523d35ead1
Nicacio Basilio <nicacioufrn@gmail.com>
2022-11-03 00:32:13 -0300

    fix: limitacao de casas decimais

--- main.js
+++ main.js
@@ -37,7 +37,6 @@ function adicionaLinha() {
         linhas += linha;
     }
 
-    
 
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
@@ -52,7 +51,7 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById("media-final-valor").innerHTML = mediaFinal;
+    document.getElementById("media-final-valor").innerHTML = mediaFinal.toFixed(2);
     document.getElementById("media-final-resultado").innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 }
